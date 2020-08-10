db3e67d7eb444dbc334729f32344109b9bf721be
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-08-11 07:42:04 +0800

    Basic R Sketch Part 2

---
+++ src/main/resources/scripts/r/BasicRunner.R
@@ -0,0 +1,7 @@
+setwd(paste0(getwd() , "/src/main/resources/scripts/r/"))
+
+print(getwd())
+
+source("Statistics.R")
+
+print(customMean())
--- src/main/resources/scripts/r/Statistics.R
+++ src/main/resources/scripts/r/Statistics.R
@@ -1,3 +1,3 @@
-customMean <- function(vector) {
+customMean <- function() {
   return (quantile(c(1,2,5,6,7,8,9,2),0.005))
 }
