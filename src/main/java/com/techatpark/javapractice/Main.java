2bdf7064e1940c5b757e136fbf4bf4a4f46dc815
haripriya <rm.haripriya@gmail.com>
2022-08-12 05:42:45 +0530

    Code Coverage Not working while running kacoco from command line

--- src/main/java/com/techatpark/javapractice/Main.java
+++ src/main/java/com/techatpark/javapractice/Main.java
@@ -1,8 +1,5 @@
 package com.techatpark.javapractice;
 
-
-import java.util.List;
-
 /**
  * Main Class.
  */
@@ -10,26 +7,11 @@ public final class Main {
 
     /**
      * Say Hello.
-     * @param list
+     * @param name
      * @return Hellotext
      */
-    public String sayHello(final List<String> list) {
-        //sorting with asc by char length
-        list.sort((o1, o2) -> o1.length() > o2.length() ?  1 : -1);
-        //geting first element for comparing rest of elements ina array
-        char[] c = list.get(0).toCharArray();
-        for (int i = 0; i < c.length; i++) {
-            for (int j = 0; j < list.size(); j++) {
-                //comparing  char,
-                // when the char fails
-                // it will return the substring of the breaking point
-                if (c[i] != list.get(j).charAt(i)) {
-                    return  j == 0 ? "" : list.get(j).substring(0, i);
-                }
-            }
-        }
-
-        return "";
+    public String sayHello(final String name) {
+        return "Hello " + name;
     }
 
 }
--- src/test/java/com/techatpark/javapractice/MainTest.java
+++ src/test/java/com/techatpark/javapractice/MainTest.java
@@ -2,11 +2,6 @@ package com.techatpark.javapractice;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -19,15 +14,8 @@ class MainTest {
 
     @Test
     void sayHello() {
-        //approch for comparing char
-//        {"geeksforgeeks", "geeks", "geek", "geezer"}
-        assertEquals("gee",main.sayHello(new ArrayList<String>(Arrays.asList("geeks for geeks", "geeks","geek","geezer"))));
-//        {"apple", "ape", "april"}
-        assertEquals("ap"
-                ,main.sayHello( new ArrayList<String>(Arrays.asList("apple", "ape", "april"))));
-//        {“apple”, “ape”, “zebra”}
-        assertEquals(""
-                ,main.sayHello( new ArrayList<String>(Arrays.asList("apple", "ape", "zebra"))));
+        assertEquals("Hello world"
+                ,main.sayHello("world"));
     }
 
 }
