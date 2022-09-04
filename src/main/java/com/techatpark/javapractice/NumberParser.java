58ae267fb6e49cec2cd97fd4070b7013daf1ff98
sathishk <sathishkumar.thiyagarajan@gmail.com>
2022-09-04 12:13:03 +0530

    Byte Short.

--- src/main/java/com/techatpark/javapractice/NumberParser.java
+++ src/main/java/com/techatpark/javapractice/NumberParser.java
@@ -12,6 +12,35 @@ public class NumberParser {
      */
     public Number parser(final String source)
             throws ParseException {
+
+
+        int length = source.length();
+
+        switch (length) {
+            case 1,2:
+                return Byte.parseByte(source);
+            case 3:
+                return isByte(source) ? Byte.parseByte(source) : Short.parseShort(source);
+            case 4,5,6:
+                return Short.parseShort(source);
+        }
+
         throw new UnsupportedOperationException("Yet to implement");
+    }
+
+    private boolean isByte(String source) {
+        int charZero = Character.getNumericValue(source.charAt(0));
+        if(charZero == 1) {
+            int charOne = Character.getNumericValue(source.charAt(1));
+            if(charOne < 3) {
+                int charTwo = Character.getNumericValue(source.charAt(2));
+                if(charTwo != 8 && charTwo != 9) {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+        return false;
     }
 }
--- src/test/java/com/techatpark/javapractice/NumberParserTest.java
+++ src/test/java/com/techatpark/javapractice/NumberParserTest.java
@@ -10,10 +10,20 @@ class NumberParserTest {
     @Test
     void parserNumber() throws ParseException {
         NumberParser parser = new NumberParser();
-        Assertions.assertEquals(parser.parser("127").getClass(), Byte.class ,
+
+        Assertions.assertEquals(parser.parser("12").getClass(), Byte.class ,
                 "Byte not identified properly");
 
-        Assertions.assertEquals(parser.parser("128").getClass(), Short.class ,
+        Assertions.assertEquals(parser.parser("1283").getClass(), Short.class ,
+                "Short not identified properly");
+
+        Assertions.assertEquals(parser.parser("245").getClass(), Short.class ,
+                "Short not identified properly");
+
+        Assertions.assertEquals(parser.parser("135").getClass(), Short.class ,
+                "Short not identified properly");
+
+        Assertions.assertEquals(parser.parser("129").getClass(), Short.class ,
                 "Short not identified properly");
     }
 }
