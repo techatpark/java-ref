099fdc5a5f67b0019fb24aa18af4cd12716fd296
haripriya <rm.haripriya@gmail.com>
2022-09-05 18:19:28 +0530

    Big Int

--- src/test/java/com/techatpark/javapractice/NumberParserTest.java
+++ src/test/java/com/techatpark/javapractice/NumberParserTest.java
@@ -3,6 +3,7 @@ package com.techatpark.javapractice;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
 import java.text.ParseException;
 
 class NumberParserTest {
@@ -26,6 +27,16 @@ class NumberParserTest {
                 Integer.class,
                 parser.parser(Integer.toString(Integer.MAX_VALUE)),
                 "Integer Max not identified properly");
+
+        Assertions.assertInstanceOf(
+                Long.class,
+                parser.parser(Long.toString(Long.MAX_VALUE)),
+                "Long Max not identified properly");
+
+        Assertions.assertInstanceOf(
+                BigInteger.class,
+                parser.parser(BigInteger.valueOf(Long.MAX_VALUE).multiply(BigInteger.TWO).toString()),
+                "BigInteger Max not identified properly");
 
 
     }
