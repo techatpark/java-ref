b4d76519360bb000c8d146133b28c7ba66b72cd3
thirumurugan mani <thirumurugan3131@gmail.com>
2022-09-06 08:33:04 +0530

    number parser Bing integer

--- src/main/java/com/techatpark/javapractice/NumberParser.java
+++ src/main/java/com/techatpark/javapractice/NumberParser.java
@@ -1,5 +1,6 @@
 package com.techatpark.javapractice;
 
+import java.math.BigInteger;
 import java.text.ParseException;
 
 public class NumberParser {
@@ -12,7 +13,6 @@ public class NumberParser {
      */
     public Number parser(final String source)
             throws ParseException {
-
         switch (source.length()) {
             case 1, 2:
                 return Byte.parseByte(source);
@@ -35,10 +35,40 @@ public class NumberParser {
                     return Integer.parseInt(source);
                 }
                 return Long.parseLong(source);
+            case 11,12,13,14,15,16,17,18:
+                return Long.parseLong(source);
+            case 19:
+                if(isLong(source)) {
+                    return Long.parseLong(source);
+                }
+                return new BigInteger(source);
             default:
-                throw new UnsupportedOperationException("Yet to implement");
+                return new BigInteger(source);
+//                throw new UnsupportedOperationException("Yet to implement");
         }
 
+    }
+
+    //9223372036854775807
+    private boolean isLong(String source) {
+        return  Character.getNumericValue(source.charAt(1)) < 3
+                && Character.getNumericValue(source.charAt(2)) < 3
+                && Character.getNumericValue(source.charAt(3)) < 4
+                && Character.getNumericValue(source.charAt(4)) < 4
+                && Character.getNumericValue(source.charAt(5)) < 8
+                && Character.getNumericValue(source.charAt(6)) < 3
+                && Character.getNumericValue(source.charAt(7)) == 0
+                && Character.getNumericValue(source.charAt(8)) < 4
+                && Character.getNumericValue(source.charAt(9)) < 7
+                && Character.getNumericValue(source.charAt(10)) < 9
+                && Character.getNumericValue(source.charAt(11)) < 6
+                && Character.getNumericValue(source.charAt(12)) < 5
+                && Character.getNumericValue(source.charAt(13)) < 8
+                && Character.getNumericValue(source.charAt(14)) < 8
+                && Character.getNumericValue(source.charAt(15)) < 6
+                && Character.getNumericValue(source.charAt(16)) < 9
+                && Character.getNumericValue(source.charAt(17)) == 0
+                && Character.getNumericValue(source.charAt(18)) < 8;
     }
 
     // 127
@@ -73,6 +103,4 @@ public class NumberParser {
                 && Character.getNumericValue(source.charAt(8)) < 5
                 && !(lastDigit == 8 || lastDigit == 9);
     }
-
-
 }
